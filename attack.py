6d7bb832be965a1ae3319e04012a5092c00b5bc9
IncludeArthur <arturo.bellin@gmail.com>
2020-11-01 20:07:48 +0100

    added task 4

--- attack.py
+++ attack.py
@@ -3,26 +3,39 @@ from hexutils import *
 
 
 def find_mat(enc, l):
+	a = np.zeros((l, l), dtype=int)
+	b = np.zeros((l, l), dtype=int)
 
-    a = np.zeros((l, l), dtype=int)
-    b = np.zeros((l, l), dtype=int)
+	# compute matrix A
+	u = strhex_to_bin_array('0x00000000')
+	for j in range(l):
+		e = np.zeros((l,), dtype=int)
+		e[j] = 1
+		x = enc(u, e, 17)
+		x = x.reshape((32, 1))
+		a[:, j] = x[:, 0]
 
-    # compute matrix A
-    u = strhex_to_bin_array('0x00000000')
-    for j in range(l):
-        e = np.zeros((l,), dtype=int)
-        e[j] = 1
-        x = enc(u, e, 17)
-        x = x.reshape((32, 1))
-        a[:, j] = x[:, 0]
+	# compute matrix B
+	k = strhex_to_bin_array('0x00000000')
+	for j in range(l):
+		e = np.zeros((l,), dtype=int)
+		e[j] = 1
+		x = enc(e, k, 17)
+		x = x.reshape((32, 1))
+		b[:, j] = x[:, 0]
 
-    # compute matrix B
-    k = strhex_to_bin_array('0x00000000')
-    for j in range(l):
-        e = np.zeros((l,), dtype=int)
-        e[j] = 1
-        x = enc(e, k, 17)
-        x = x.reshape((32, 1))
-        b[:, j] = x[:, 0]
+	return a, b
 
-    return a, b
+
+def find_key_kpa(a, b, u, x):
+	a_inv = np.linalg.inv(a)
+	a_det = np.linalg.det(a)
+	a1 = (a_inv * a_det)
+	a1 = np.mod(a1, 2)
+	#print("inverted a:")
+	#print(a1)
+	k = np.dot(a1, (x + np.dot(b, u)))
+	k = np.rint(k).astype(int)  # the previous arrays are all float with some errors
+	#print("key found: ", k)
+
+	return np.mod(k, 2)
--- main.py
+++ main.py
@@ -4,79 +4,95 @@ from attack import *
 
 
 def key_gen(i, k):
-    l_k = k.shape[0]
-    key = np.zeros((l_k,), dtype=int)
+	l_k = k.shape[0]
+	key = np.zeros((l_k,), dtype=int)
 
-    for j in range(1, l_k+1):
-        index = ((5 * i + j - 1) % l_k) + 1  # the formula in the slide is 1-indexed
-        key[j-1] = k[index-1]
+	for j in range(1, l_k+1):
+		index = ((5 * i + j - 1) % l_k) + 1  # the formula in the slide is 1-indexed
+		key[j-1] = k[index-1]
 
-    return key
+	return key
 
 
 def f(k, i, y):
-    l = y.shape[0]
-    w = np.zeros((l+1,), dtype=int)  # w is starts from index 1 for easier computation
-    for j in range(1, l+1):
-        if j <= l / 2:
-            w[j] = y[j-1] ^ k[4*j - 3 -1]
-        else:
-            w[j] = y[j-1] ^ k[4*j - 2*l -1]
-    return w[1:]
+	l = y.shape[0]
+	w = np.zeros((l+1,), dtype=int)  # w is starts from index 1 for easier computation
+	for j in range(1, l+1):
+		if j <= l / 2:
+			w[j] = y[j-1] ^ k[4*j - 3 -1]
+		else:
+			w[j] = y[j-1] ^ k[4*j - 2*l -1]
+	return w[1:]
 
 
 def encrypt(u, k, r):
-    y = u[:16]
-    z = u[16:]
+	y = u[:16]
+	z = u[16:]
 
-    for i in range(r):
-        round_key = key_gen(i+1, k)
-        w = f(round_key, i, y)
-        v = np.bitwise_xor(w, z)
-        z = y
-        y = v
+	for i in range(r):
+		round_key = key_gen(i+1, k)
+		w = f(round_key, i, y)
+		v = np.bitwise_xor(w, z)
+		z = y
+		y = v
 
-    x = np.append(z, y)  # wrong way around since we did one more T in the for loop
-    return x
+	x = np.append(z, y)  # wrong way around since we did one more T in the for loop
+	return x
 
 
 def decrypt(x, k, r):
-    y = x[:16]
-    z = x[16:]
+	y = x[:16]
+	z = x[16:]
 
-    for i in range(r):
-        round_key = key_gen(r-i, k)  # inverted key sequence
-        w = f(round_key, i, y)
-        v = np.bitwise_xor(w, z)
-        z = y
-        y = v
+	for i in range(r):
+		round_key = key_gen(r-i, k)  # inverted key sequence
+		w = f(round_key, i, y)
+		v = np.bitwise_xor(w, z)
+		z = y
+		y = v
 
-    u = np.append(z, y)  # wrong way around since we did one more T in the for loop
-    return u
+	u = np.append(z, y)  # wrong way around since we did one more T in the for loop
+	return u
 
 
 def main():
-    k = strhex_to_bin_array('0x80000000')
-    u = strhex_to_bin_array('0x80000000')
+	k = strhex_to_bin_array('0x80000000')
+	u = strhex_to_bin_array('0x80000000')
 
-    x = encrypt(u, k, 17)
-    uu = decrypt(x, k, 17)
+	x = encrypt(u, k, 17)
+	uu = decrypt(x, k, 17)
 
-    print("Encrypted text:")
-    #print(x)
-    print(bin_array_to_strhex(x))
-    print("Decrypted text:")
-    #print(uu)
-    print(bin_array_to_strhex(uu))
+	print("Encrypted text:")
+	#print(x)
+	print(bin_array_to_strhex(x))
+	print("Decrypted text:")
+	#print(uu)
+	print(bin_array_to_strhex(uu))
 
-    print("attack test")
-    a, b = find_mat(encrypt, 32)
+	print("attack test")
+	a, b = find_mat(encrypt, 32)
 
-    print("matrix A:")
-    print(a)
-    print("matrix B:")
-    print(b)
+	print("matrix A:")
+	print(a)
+	print("matrix B:")
+	print(b)
+	
+	print("test KPA attack")
+	tk = strhex_to_bin_array('0x12340050')
+	tx = encrypt(u, tk, 17)
+	kk = find_key_kpa(a, b, u, tx)
+	print("key: ", bin_array_to_strhex(kk))
+
+	print("KPA attack on file data")
+	file1 = open('data/KPApairsVancouver_linear.hex', 'r')
+	lines = file1.readlines()
+	for line in lines:
+		l = line.split("\t")
+		p_txt = strhex_to_bin_array("0x"+l[0])
+		c_txt = strhex_to_bin_array("0x"+l[1])
+		kk = find_key_kpa(a, b, p_txt, c_txt)
+		print("key: ", bin_array_to_strhex(kk))
 
 
 if __name__ == "__main__":
-    main()
+	main()
