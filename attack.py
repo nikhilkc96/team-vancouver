70a8490d8c4810880becf94a41f9c6a104b1a26c
IncludeArthur <arturo.bellin@gmail.com>
2020-11-04 20:26:07 +0100

    added working task 8

--- attack.py
+++ attack.py
@@ -43,22 +43,55 @@ def find_key_kpa(a, b, u, x):
 
 def meet_in_the_middle(n1, n2, enc, dec, u, x, f, l):
 	l1 = []
-	l2 = [] 
+	l2 = []
 	# generate n1 random guesses for k1 and the corresponding encrypted cyphertexts
 	while len(l1) < n1:
 		k1 = np.random.randint(0, 2, l, dtype=int)
 		x1 = enc(u, k1, 13, l, f)
-		l1.append([k1, x1])
+		l1.append([bin_array_to_strhex(k1), bin_array_to_strhex(x1)])
 	# generate n2 random guesses for k2 and the corresponding decrypted plaintexts
 	while len(l2) < n2:
 		k2 = np.random.randint(0, 2, l, dtype=int)
 		u2 = dec(x, k2, 13, l, f)
-		l2.append([k2, u2])
+		l2.append([bin_array_to_strhex(k2), bin_array_to_strhex(u2)])
 
 	# search for matches between x1 and u2
 	matches = []
-	for i in range(len(l1)):
-		for j in range(len(l2)):
-			if np.array_equal(l1[i][1], l2[j][1]):
-				matches.append([l1[i][0], l2[j][0]])
+	l1 = np.array(l1)
+	l2 = np.array(l2)
+	print(l1.shape, l2.shape)
+
+	commons, mask1, mask2 = np.intersect1d(l1[:, 1], l2[:, 1], return_indices=True)
+	print("Found matches: ", len(mask1))
+	for i in range(len(mask1)):
+		matches.append([l1[mask1[i], 0], l2[mask2[i], 0]])
+	return matches
+
+
+def meet_in_the_middle_sequential(n1, n2, enc, dec, u, x, f, l):
+	l1 = []
+	l2 = []
+	n1 = max(n1, 2 ** l)
+	n2 = max(n2, 2 ** l)
+	# generate n1 random guesses for k1 and the corresponding encrypted cyphertexts
+	for i in range(n1):
+		k1 = hex(i)
+		x1 = enc(u, strhex_to_bin_array(k1, l), 13, l, f)
+		l1.append([k1, bin_array_to_strhex(x1)])
+
+	for i in range(n2):
+		k2 = hex(i)
+		u2 = dec(x, strhex_to_bin_array(k2, l), 13, l, f)
+		l2.append([k2, bin_array_to_strhex(u2)])
+
+	# search for matches between x1 and u2
+	matches = []
+	l1 = np.array(l1)
+	l2 = np.array(l2)
+	print(l1.shape, l2.shape)
+
+	commons, mask1, mask2 = np.intersect1d(l1[:, 1], l2[:, 1], return_indices=True)
+	print("Found matches: ", len(mask1))
+	for i in range(len(mask1)):
+		matches.append([l1[mask1[i], 0], l2[mask2[i], 0]])
 	return matches
--- main.py
+++ main.py
@@ -7,56 +7,56 @@ def key_gen(i, k):
 	l_k = k.shape[0]
 	key = np.zeros((l_k,), dtype=int)
 
-	for j in range(1, l_k+1):
+	for j in range(1, l_k + 1):
 		index = ((5 * i + j - 1) % l_k) + 1  # the formula in the slide is 1-indexed
-		key[j-1] = k[index-1]
+		key[j - 1] = k[index - 1]
 
 	return key
 
 
 def lin_f(k, i, y):
 	l = y.shape[0]
-	w = np.zeros((l+1,), dtype=int)  # w is starts from index 1 for easier computation
-	for j in range(1, l+1):
+	w = np.zeros((l + 1,), dtype=int)  # w is starts from index 1 for easier computation
+	for j in range(1, l + 1):
 		if j <= l / 2:
-			w[j] = y[j-1] ^ k[4*j - 3 -1]
+			w[j] = y[j - 1] ^ k[4 * j - 3 - 1]
 		else:
-			w[j] = y[j-1] ^ k[4*j - 2*l -1]
+			w[j] = y[j - 1] ^ k[4 * j - 2 * l - 1]
 	return w[1:]
 
 
 def near_lin_f(k, i, y):
 	l = y.shape[0]
-	w = np.zeros((l+1,), dtype=int)  # w is starts from index 1 for easier computation
-	for j in range(1, l+1):
+	w = np.zeros((l + 1,), dtype=int)  # w is starts from index 1 for easier computation
+	for j in range(1, l + 1):
 		if j <= l / 2:
-			t = y[2*j - 1 -1] | k[2*j - 1 -1] | k[2*j -1] | k[4*j - 2 -1]
-			w[j] = y[j -1] ^ (k[4*j -3 -1] & t)
+			t = y[2 * j - 1 - 1] | k[2 * j - 1 - 1] | k[2 * j - 1] | k[4 * j - 2 - 1]
+			w[j] = y[j - 1] ^ (k[4 * j - 3 - 1] & t)
 		else:
-			t = k[4*j -2*l - 1 -1] | k[2*j - 1 -1] | k[2*j -1] | [2*j - l -1]
-			w[j] = y[j-1] ^ (k[4*j - 2*l -1] & t)
+			t = k[4 * j - 2 * l - 1 - 1] | k[2 * j - 1 - 1] | k[2 * j - 1] | [2 * j - l - 1]
+			w[j] = y[j - 1] ^ (k[4 * j - 2 * l - 1] & t)
 	return w[1:]
 
 
 def non_lin_f(k, i, y):
 	l = y.shape[0]
-	w = np.zeros((l+1,), dtype=int)  # w starts from index 1 for easier computation
-	for j in range(1, l+1):
+	w = np.zeros((l + 1,), dtype=int)  # w starts from index 1 for easier computation
+	for j in range(1, l + 1):
 		if j <= l / 2:
-			t = (y[2*j - 1 -1] & k[2*j -1]) | k[4*j -1]
-			w[j] = (y[j -1] & k[2*j - 1 -1]) | t
+			t = (y[2 * j - 1 - 1] & k[2 * j - 1]) | k[4 * j - 1]
+			w[j] = (y[j - 1] & k[2 * j - 1 - 1]) | t
 		else:
-			t = (k[4*j - 2*l - 1 -1] & k[2*j - 1]) | y[2*j - l -1]
-			w[j] = (y[j - 1] & k[2 * j - 1 -1]) | t
+			t = (k[4 * j - 2 * l - 1 - 1] & k[2 * j - 1]) | y[2 * j - l - 1]
+			w[j] = (y[j - 1] & k[2 * j - 1 - 1]) | t
 	return w[1:]
 
 
 def encrypt(u, k, r, l, f):
-	y = u[:(l//2)]
-	z = u[(l//2):]
+	y = u[:(l // 2)]
+	z = u[(l // 2):]
 
 	for i in range(r):
-		round_key = key_gen(i+1, k)
+		round_key = key_gen(i + 1, k)
 		w = f(round_key, i, y)
 		v = np.bitwise_xor(w, z)
 		z = y
@@ -67,11 +67,11 @@ def encrypt(u, k, r, l, f):
 
 
 def decrypt(x, k, r, l, f):
-	y = x[:(l//2)]
-	z = x[(l//2):]
+	y = x[:(l // 2)]
+	z = x[(l // 2):]
 
 	for i in range(r):
-		round_key = key_gen(r-i, k)  # inverted key sequence
+		round_key = key_gen(r - i, k)  # inverted key sequence
 		w = f(round_key, i, y)
 		v = np.bitwise_xor(w, z)
 		z = y
@@ -81,12 +81,11 @@ def decrypt(x, k, r, l, f):
 	return u
 
 
-def most_frequent(List): 
-    return max(set(List), key = List.count)
+def most_frequent(List):
+	return max(set(List), key=List.count)
 
 
 def main():
-
 	# ----- TASK 1 AND 2 ----
 	print("Linear Feistel")
 	k = strhex_to_bin_array('0x80000000', 32)
@@ -96,10 +95,10 @@ def main():
 	uu = decrypt(x, k, 17, 32, lin_f)
 
 	print("Encrypted text:")
-	#print(x)
+	# print(x)
 	print(bin_array_to_strhex(x))
 	print("Decrypted text:")
-	#print(uu)
+	# print(uu)
 	print(bin_array_to_strhex(uu))
 
 	# ----- TASK 3 ----
@@ -123,8 +122,8 @@ def main():
 	lines = file1.readlines()
 	for line in lines:
 		l = line.split("\t")
-		p_txt = strhex_to_bin_array("0x"+l[0], 32)
-		c_txt = strhex_to_bin_array("0x"+l[1], 32)
+		p_txt = strhex_to_bin_array("0x" + l[0], 32)
+		c_txt = strhex_to_bin_array("0x" + l[1], 32)
 		kk = find_key_kpa(a, b, p_txt, c_txt)
 		print("key: ", bin_array_to_strhex(kk))
 
@@ -159,24 +158,45 @@ def main():
 	print("Keypairs candidates")
 	total_matches = []
 	file1 = open('data/KPApairsVancouver_non_linear.hex', 'r')
+	#file1 = open('data/KPApairsTest_non_linear.hex', 'r')
 	lines = file1.readlines()
-	for line in lines:
-		l = line.split("\t")
-		p_txt = strhex_to_bin_array("0x"+l[0], 16)
-		c_txt = strhex_to_bin_array("0x"+l[1], 16)
 
-		# matches = meet_in_the_middle(1000, 1000, encrypt, decrypt, p_txt, c_txt, non_lin_f, 16)
-		matches = meet_in_the_middle(1000, 1000, encrypt, decrypt, p_txt, c_txt, non_lin_f, 16)
-		if not matches:
-			print("no matches found")
-		else:
-			for keypair in matches:
-				print("keypair: ", bin_array_to_strhex(keypair[0]), bin_array_to_strhex(keypair[1]))
-				total_matches.append(bin_array_to_strhex(keypair[0]) + " " + bin_array_to_strhex(keypair[1]))
-		
-	print("\nMost probable keypair")
-	print("keypair: ", most_frequent(total_matches))
-	
+	l = lines[0].split("\t")
+	p_txt = strhex_to_bin_array("0x" + l[0], 16)
+	c_txt = strhex_to_bin_array("0x" + l[1], 16)
+
+	# use meet_in_the_middle if you are feeling lucky today
+	# or use meet_in_the_middle_sequential if you want to be sure to get the keys
+	matches = meet_in_the_middle(2 ** 16, 2 ** 16, encrypt, decrypt, p_txt, c_txt, non_lin_f, 16)
+	correct = []
+
+	if not matches:
+		print("no matches found")
+	else:
+		for keypair in matches:
+			#if keypair[0] == '0x1234':
+			#	print('the key is present: ', keypair)
+
+			works = 0
+			for line in lines:
+				l = line.split("\t")
+				p_txt = strhex_to_bin_array("0x" + l[0], 16)
+				c_txt = strhex_to_bin_array("0x" + l[1], 16)
+				if np.array_equal(encrypt(encrypt(p_txt, strhex_to_bin_array(keypair[0], 16), 13, 16, non_lin_f),
+										  strhex_to_bin_array(keypair[1], 16), 13, 16, non_lin_f), c_txt):
+					works += 1
+			# print("Works for {} texts".format(works))
+			correct.append(works)
+
+	max_correct = max(correct)
+	index_correct = correct.index(max_correct)
+	keys_correct = matches[index_correct]
+	print("Best keypair {} with {} matches".format(keys_correct, max_correct))
+	# print(np.argmax(np.array(correct)).shape)
+	(unique, counts) = np.unique(correct, return_counts=True)
+	frequencies = np.asarray((unique, counts)).T
+	print("Histogram of matches for keypairs:")
+	print(frequencies)
 
 
 if __name__ == "__main__":
